98f5c82e7143974749b01a066642b40da3e4681e
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-03-01 17:14:53 +0900

    Default.

---
+++ Nekoja2/Nekoja2.cpp
@@ -0,0 +1,24 @@
+// Nekoja2.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+
+
+int main()  // NOLINT(bugprone-exception-escape)
+{	
+	int x = 123456 + 78910;
+	int y = 123 * 456;
+
+	cout << "MAX : ";
+	cout << x << endl;
+	cout << "MIN : ";
+	cout << y << endl;
+	cout << "" << endl;
+	
+	if (x || y)
+		cout << x << endl;
+	else
+		cout << y << endl;
+	    cout << "" << endl;
+	return 0;
+}
