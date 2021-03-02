b5e5dd0a5eb44a34ff891d92a50390c997344867
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-03-02 13:12:09 +0900

    Update, comment add.

--- Nekoja2/Nekoja2.cpp
+++ Nekoja2/Nekoja2.cpp
@@ -3,17 +3,16 @@
 
 #include "stdafx.h"
 
-
 int main()  // NOLINT(bugprone-exception-escape)
 {	
     int cpp_ver = __cplusplus;
     int cpp_eql = 199711;
-	
+
 	switch ( cpp_ver == cpp_eql ){
 	  case 1:
-	   cout << "" << endl;
-       cout << "Œ»Ý‚Ì C++ : C++98" << endl;
-	   cout << "" << endl;
+	   cout << "  ----------------------  " << endl;
+       cout << "    Œ»Ý‚Ì C++ : C++98  " << endl;
+	   cout << "  ---------------------- " << endl;
 	   break;
 	  default:
 	   cout << cpp_ver << endl;
